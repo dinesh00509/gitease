91646eb2ad5b5d30a247edcdfbd9996da33d5e02
dinesh00509 <dinzzdluffy005@gmail.com>
2025-11-10 15:21:01 +0530

    added --help tag

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
+		fmt.Println("GitEase - Interactive Git Assistant")
+		fmt.Println("Usage: gitease")
+		fmt.Println("Navigate and manage Git tasks interactively in your terminal.")
+		os.Exit(0)
+	}
 	p := tea.NewProgram(internals.IntialModel())
 
 	if err := p.Start(); err != nil {
